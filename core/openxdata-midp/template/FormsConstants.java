d6979465e52b9875a5418195d5f07d1eaa6afc71
dagmar@cell-life.org.za <dagmar@cell-life.org.za@6cddc80d-1c40-44cd-927a-2be6005847d2>
2010-05-19 07:07:00 +0000

    Adding the FormsConstants missing template
    git-svn-id: https://trac.openxdata.org/svn/openxdata/J2ME/trunk@1849 6cddc80d-1c40-44cd-927a-2be6005847d2

---
+++ core/openxdata-midp/template/FormsConstants.java
@@ -0,0 +1,18 @@
+package org.openxdata.forms;
+
+/**
+ * This class contains properties specifying configurable UI settings
+ * such as "Single Question Edit"
+ * 
+ * See the ant file build-configuration.xml that replaces the tokens
+ * with the values in the antbuild.properties and creates the class 
+ * in the correct package so it can be used in the application.
+ *  
+ * @author dagmar@cell-life.org
+ */
+public class FormsConstants  {
+	
+	public static final String SINGLE_QUESTION_EDIT = "@single.question.edit@";
+	public static final String QUESTION_NUMBERING = "@question.numbering@";
+	public static final String OK_ON_RIGHT = "@ok.on.right@";
+}
