1d73c089d5ed62ac67bbc7a51afef147a39adc68
kayiwadaniel@gmail.com <kayiwadaniel@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2010-06-11 12:08:37 +0000

    Prevent re-selection of study: prevents OOME with large study. From MoTeCH revision 61
    git-svn-id: https://trac.openxdata.org/svn/openxdata/J2ME/trunk@2008 6cddc80d-1c40-44cd-927a-2be6005847d2

--- core/openxdata-midp/src/org/openxdata/forms/StudyListView.java
+++ core/openxdata-midp/src/org/openxdata/forms/StudyListView.java
@@ -23,40 +23,40 @@ import org.openxdata.util.MenuText;
  */
 public class StudyListView extends AbstractView implements CommandListener  {
 	private Vector studyList;
-	
+
 	public StudyListView(){
-		
+
 	}
-	
+
 	public void showStudyList(Vector studyList){
 		this.studyList = studyList;
-		
+
 		screen = new List(MenuText.SELECT_STUDY()+" - "+title , Choice.IMPLICIT);
 		((List)screen).setFitPolicy(List.TEXT_WRAP_ON);
-		
-        StudyDef study; int selectedIndex = OpenXdataConstants.NO_SELECTION;
+
+		StudyDef study; int selectedIndex = OpenXdataConstants.NO_SELECTION;
 		Settings settings = new Settings(OpenXdataConstants.STORAGE_NAME_EPIHANDY_SETTINGS,true);
 		String val = settings.getSetting(OpenXdataConstants.KEY_LAST_SELECTED_STUDY);
-		
+
 		for(int i=0; i<studyList.size(); i++){
 			study = (StudyDef)studyList.elementAt(i);
 			if(selectedIndex == OpenXdataConstants.NO_SELECTION && val != null){
 				if(study.getId() == Byte.parseByte(val))
 					selectedIndex = i;
 			}
-			
+
 			((List)screen).append(study.getName(), null);
 		}
-		
+
 		if(selectedIndex != OpenXdataConstants.NO_SELECTION)
 			((List)screen).setSelectedIndex(selectedIndex, true);
-		
+
 		screen.setCommandListener(this);
 		screen.addCommand(DefaultCommands.cmdOk);
 		screen.addCommand(DefaultCommands.cmdCancel);
 		display.setCurrent(screen);
 	}
-	
+
 	/**
 	 * Processes the command events.
 	 * 
@@ -65,17 +65,28 @@ public class StudyListView extends AbstractView implements CommandListener  {
 	 */
 	public void commandAction(Command c, Displayable d) {
 		try{
-			if(c == List.SELECT_COMMAND || c == DefaultCommands.cmdOk)
+			//This block is commented out to have the next block from MoTeCH
+			/*if(c == List.SELECT_COMMAND || c == DefaultCommands.cmdOk)
 				getOpenXdataController().execute(this,DefaultCommands.cmdOk,(StudyDef)studyList.elementAt(((List)d).getSelectedIndex()));
 			else if(c == DefaultCommands.cmdCancel)
-				getOpenXdataController().execute(this,DefaultCommands.cmdCancel,null);
+				getOpenXdataController().execute(this,DefaultCommands.cmdCancel,null);*/
+
+			OpenXdataController controller = getOpenXdataController();
+			if (c == List.SELECT_COMMAND || c == DefaultCommands.cmdOk) {
+				StudyDef selectedStudy = (StudyDef) studyList.elementAt(((List) d).getSelectedIndex());
+				StudyDef currentStudy = controller.getCurrentStudy();
+				if (selectedStudy != null && selectedStudy.getId() != currentStudy.getId())
+					controller.execute(this, DefaultCommands.cmdOk, selectedStudy);
+			} 
+			else if (c == DefaultCommands.cmdCancel)
+				controller.execute(this, DefaultCommands.cmdCancel, null);
 		}
 		catch(Exception e){
 			//alertMsg.showError(e.getMessage());
 			//e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Processes the cancel command event.
 	 * 
@@ -84,7 +95,7 @@ public class StudyListView extends AbstractView implements CommandListener  {
 	/*private void handleCancelCommand(Displayable d){
 		getEpihandyController().execute(this,DefaultCommands.cmdCancel,null);
 	}
-	
+
 	/**
 	 * Processes the OK command event.
 	 * 
@@ -94,15 +105,15 @@ public class StudyListView extends AbstractView implements CommandListener  {
 		StudyDef study = (StudyDef)studyList.elementAt(((List)d).getSelectedIndex());
 		getEpihandyController().execute(this,DefaultCommands.cmdOk,study);
 	}*/
-	
+
 	public void setStudyList(Vector list){
 		studyList = list;
 	}
-	
+
 	public Vector getStudyList(){
 		return studyList;
 	}
-	
+
 	private OpenXdataController getOpenXdataController(){
 		return (OpenXdataController)controller;
 	}
