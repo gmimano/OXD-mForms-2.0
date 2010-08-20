6ee259543d4904846090b486ef002461e981501e
cattabanks@gmail.com <cattabanks@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2010-08-20 14:15:15 +0000

    Minor clean up
    git-svn-id: https://trac.openxdata.org/svn/openxdata/J2ME/trunk@2745 6cddc80d-1c40-44cd-927a-2be6005847d2

--- core/jzlib-1.0.7/src/com/jcraft/jzlib/InfTree.java
+++ core/jzlib-1.0.7/src/com/jcraft/jzlib/InfTree.java
@@ -39,15 +39,9 @@ final class InfTree{
   static final private int MANY=1440;
 
   static final private int Z_OK=0;
-  static final private int Z_STREAM_END=1;
-  static final private int Z_NEED_DICT=2;
-  static final private int Z_ERRNO=-1;
-  static final private int Z_STREAM_ERROR=-2;
   static final private int Z_DATA_ERROR=-3;
   static final private int Z_MEM_ERROR=-4;
   static final private int Z_BUF_ERROR=-5;
-  static final private int Z_VERSION_ERROR=-6;
-
   static final int fixed_bl = 9;
   static final int fixed_bd = 5;
 
