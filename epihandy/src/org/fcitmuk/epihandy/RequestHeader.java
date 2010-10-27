c2a62cbedae14276a8404ff90801cfab3539100e
dagmar@cell-life.org.za <dagmar@cell-life.org.za@6cddc80d-1c40-44cd-927a-2be6005847d2>
2010-10-27 12:16:19 +0000

    Change serializer to match release for mforms-proto
    git-svn-id: https://trac.openxdata.org/svn/openxdata/J2ME/trunk@3206 6cddc80d-1c40-44cd-927a-2be6005847d2

--- epihandy/src/org/fcitmuk/epihandy/RequestHeader.java
+++ epihandy/src/org/fcitmuk/epihandy/RequestHeader.java
@@ -49,7 +49,7 @@ public class RequestHeader implements Persistent{
 
 	private String userName = EpihandyConstants.EMPTY_STRING;
 	private String password = EpihandyConstants.EMPTY_STRING;
-	private static String serializer = "mforms-proto-1.0-SNAPSHOT";
+	private static String serializer = "mforms-proto-1.0";
 	private String locale = "en";
 
 
